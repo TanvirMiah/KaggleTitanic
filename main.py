21ccff40a24f03860a185cffd3e3478e7922557f
TanvirMiah <alaminmiah95@hotmail.co.uk>
2020-03-11 17:13:05 +0000

    Need to clean Fares and then ready for analysis

--- main.py
+++ main.py
@@ -180,5 +180,110 @@ for dataset in fullset:
 train.head(10)
 
 '''
-Fill in gaps between information
+Fill in null values
 '''
+
+'''
+Multiple ways to approach this: 
+1. Generate random numbers from standard deviation and mean
+2. Guess the missing values from other values from the data (like correlatinh Age, Gender and PClass)
+3. Do a bit of both
+
+Method 1 and 3 generate randomness in our models, so best to avoid those.
+'''
+
+# visualise the correlation between Pclass and gender against age
+pclass_age_corr = sns.FacetGrid(train, row='Pclass', col='Sex', size=2.2, aspect=1.6)
+pclass_age_corr.map(plt.hist, 'Age', alpha =.9, bins=20)
+pclass_age_corr.add_legend()
+
+# write observations made
+
+# using Pclass and Gender, we can guess ages
+# create an empty array to guess ages
+guess_ages = np.zeros((2,3))
+
+# run a for loop that calculates the median age of the average male and female in 
+# Pclase 1, 2 and 3, and then replace the missing ages
+for dataset in fullset:
+    for i in range(0, 2):
+        for j in range(0, 3):
+            guess_df = dataset[(dataset['Sex'] == i) & \
+                                  (dataset['Pclass'] == j+1)]['Age'].dropna()
+
+            age_guess = guess_df.median()
+
+            # Convert random age float to nearest .5 age
+            guess_ages[i,j] = int( age_guess/0.5 + 0.5 ) * 0.5
+            
+    for i in range(0, 2):
+        for j in range(0, 3):
+            dataset.loc[ (dataset.Age.isnull()) & (dataset.Sex == i) & (dataset.Pclass == j+1),\
+                    'Age'] = guess_ages[i,j]
+
+    dataset['Age'] = dataset['Age'].astype(int)
+
+# check everything worked
+train.head(10)
+
+# put ages into discrete bins, using pandas cut feature
+age_bands = train['AgeBand'] = pd.cut(train['Age'], 5)
+age_bands = train[['AgeBand', 'Survived']].groupby(['AgeBand'], as_index=False).mean().sort_values(by='AgeBand', ascending=True)
+
+# now that we have the age bands, lets turn them into ordinals
+for dataset in fullset:    
+    dataset.loc[ dataset['Age'] <= 16, 'Age'] = 0
+    dataset.loc[(dataset['Age'] > 16) & (dataset['Age'] <= 32), 'Age'] = 1
+    dataset.loc[(dataset['Age'] > 32) & (dataset['Age'] <= 48), 'Age'] = 2
+    dataset.loc[(dataset['Age'] > 48) & (dataset['Age'] <= 64), 'Age'] = 3
+    dataset.loc[ dataset['Age'] > 64, 'Age']
+
+# check everything is ok
+train.head(10)
+
+# the AgeBand feature is not needed anymore
+train = train.drop(['AgeBand'], axis=1)
+fullset = [train, test]
+train.head(10)
+
+'''
+Combine the Parch and Siblings data to a single datapoint
+'''
+for dataset in fullset:
+    dataset['FamilySize'] = dataset['SibSp'] + dataset['Parch'] + 1
+    
+family_size = train[['FamilySize', 'Survived']].groupby(['FamilySize'], as_index=False).mean().sort_values(by='Survived', ascending=False)
+
+# add an additonal feature to see if being alone makes a difference
+for dataset in fullset:
+    dataset['IsAlone'] = 0
+    dataset.loc[dataset['FamilySize'] == 1, 'IsAlone'] = 1
+    
+is_alone_stats = train[['IsAlone', 'Survived']].groupby(['IsAlone'], as_index = False).mean()
+
+# parch and sibsp and family size can be dropped as they are not needed, as IsAlone covers it
+train = train.drop(['Parch', 'SibSp', 'FamilySize'], axis = 1)
+test = test.drop(['Parch', 'SibSp', 'FamilySize'], axis = 1)
+fullset = [train, test]
+
+# check everything is ok
+train.head(10)
+
+'''
+Turn the embarked feature into ordinal numbers
+'''
+
+# two NA's in the embarked training data
+# find the most common Embarked place and replace the two NA's with that 
+freq_port = train.Embarked.dropna().mode()[0]
+
+for dataset in fullset:
+    dataset['Embarked'] = dataset['Embarked'].fillna(freq_port)
+
+embarked_freq = train[['Embarked', 'Survived']].groupby(['Embarked'], as_index=False).mean().sort_values(by='Survived', ascending=False)
+
+# convert Embarked into ordinal numbers
+for dataset in fullset:
+    dataset['Embarked'] = dataset['Embarked'].map({'S':0, 'C':1, 'Q':2}).astype(int)
+    
+train.head(10)
